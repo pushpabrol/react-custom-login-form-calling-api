ee6f80f046ae6dbd40e712505ca6998b60250ece
pushpabrol <pushp.abrol@gmail.com>
2017-08-14 10:55:53 -0400

    first commit

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'tenant.auth0.com',
+  clientId: 'client_id',
+  callbackUrl: 'callback_url',
+  apiUrl: 'api_urn'
+}
---
+++ src/Home/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class Home extends Component {
+  login() {
+    this.props.auth.login();
+  }
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div className="container">
+        {
+          isAuthenticated() && (
+              <h4>
+                You are logged in!
+              </h4>
+            )
+        }
+        {
+          !isAuthenticated() && (
+              <h4>
+                You are not logged in! Please{' '}
+                <Link
+                  to={'/login'}
+                >
+                  Log In
+                </Link>
+                {' '}to continue.
+              </h4>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default Home;
